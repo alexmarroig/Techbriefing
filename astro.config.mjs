00ff0dff2aed5e9740cbc867c6899d6598303089
alexmarroig <alex.c.marroig@gmail.com>
2026-05-12 17:22:38 -0300

    Update production domain and Google tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://techbriefing.com.br',
+  site: 'https://www.techbriefing.com.br',
   integrations: [react(), sitemap()],
   vite: {
     esbuild: {
